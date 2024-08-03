6488acace33d607948ecc1cc3a817b6c21e37c9c
yuyu591 <257476140+yuyu591@users.noreply.github.com>
2024-08-03 07:46:45 +0000

    [abc083] abc083_a

---
+++ abc083/abc083_a/abc083_a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b,c,d;
+  cin >> a >> b >> c >> d;
+  
+  int l = a + b;
+  int r = c + d;
+  if (l > r) {
+    cout << "Left" << endl;
+  } else if (l == r) {
+    cout << "Balanced" << endl;
+  } else {
+    cout << "Right" << endl;
+  }
+}
