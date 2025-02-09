cd7006dffe53e6b880813534e79f3ddd0f8cb0ce
yuyu591 <257476140+yuyu591@users.noreply.github.com>
2025-02-09 18:35:43 +0000

    [abc015] abc015_1

---
+++ abc015/abc015_1/abc015_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+
+    if (a.size() > b.size()) {
+        cout << a << endl;
+    } else {
+        cout << b << endl;
+    }
+}
