bcd293524276df5d9a5b5d24d5e9ad710135a281
yuyu591 <257476140+yuyu591@users.noreply.github.com>
2025-02-13 02:19:30 +0000

    [abc376] abc376_a

--- abc376/abc376_a/abc376_a.cpp
+++ abc376/abc376_a/abc376_a.cpp
@@ -2,21 +2,18 @@
 using namespace std;
 
 int main() {
-    int n,c,t;
+    int n,c;
     cin >> n >> c;
   
-    int t0 = 0; //t0は前回飴をもらった時間
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
+    int pre, cnt = 1; //preは前回飴をもらった時間、最初の1回は確定でもらえるから初期値1
+    cin >> pre; //1つ目のtを直接preに代入することで綺麗に進む！
+
+    for (int i = 1; i < n; i++) {
+        int t;
         cin >> t;
-        if (i == 0) { //最初の1回は確定でもらえる
+        if (t - pre >= c) {
             cnt++;
-            t0 = t;
-        } else {
-            if (t - t0 >= c) {
-                cnt++;
-                t0 = t;
-            }
+            pre = t;
         }
     }
     cout << cnt << endl;
