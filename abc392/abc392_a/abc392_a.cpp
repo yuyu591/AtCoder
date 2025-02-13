62e598e2fb18305e544dec51d7f1fa14fe497677
yuyu591 <257476140+yuyu591@users.noreply.github.com>
2025-02-13 08:06:14 +0000

    [abc392] abc392_a

---
+++ abc392/abc392_a/abc392_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a1,a2,a3;
+  cin >> a1 >> a2 >> a3;
+  
+  if ((a1 * a2 == a3) || (a1 * a3 == a2) || (a2 * a3 == a1)) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
